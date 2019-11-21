db15b4db37e407fc36f1e06622915ec55619216b
Володимир Сологубов <wova607@hotmail.com>
2019-11-21 23:04:54 +0200

    some do

--- AngulareClient/angular-client/src/app/app.module.ts
+++ AngulareClient/angular-client/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CategoryListComponent } from './category-list/category-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryListComponent
+    
   ],
   imports: [
     BrowserModule
